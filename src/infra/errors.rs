7d6950da5b4df8ed1dda63a6df9716deca8b5ab3
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2023-09-28 20:02:53 +0530

    fix: pool error logging

--- src/infra/errors.rs
+++ src/infra/errors.rs
@@ -3,23 +3,25 @@ use diesel_async::pooled_connection::deadpool::PoolError;
 
 #[derive(Debug, thiserror::Error)]
 pub enum InfraError {
-    #[error("internal server error")]
-    InternalServerError,
+    #[error("internal server error: {0}")]
+    InternalServerError(Error),
     #[error("not found")]
     NotFound,
+    #[error("pool error: {0}")]
+    PoolError(PoolError),
 }
 
 impl From<Error> for InfraError {
     fn from(value: Error) -> Self {
         match value {
             Error::NotFound => InfraError::NotFound,
-            _ => InfraError::InternalServerError,
+            _ => InfraError::InternalServerError(value),
         }
     }
 }
 
 impl From<PoolError> for InfraError {
-    fn from(_value: PoolError) -> Self {
-        Self::InternalServerError
+    fn from(value: PoolError) -> Self {
+        Self::PoolError(value)
     }
 }
