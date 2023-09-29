9d7721e274c8276aca96527ba405eb5ca79a174d
Evolve <artevolve@yahoo.com>
2023-09-29 14:37:10 +0200

    remove useless tests

--- src/handlers/indexers/utils.rs
+++ src/handlers/indexers/utils.rs
@@ -45,34 +45,4 @@ mod tests {
 
         assert_eq!(result_key, expected_key);
     }
-
-    #[test]
-    fn test_get_script_tmp_directory_random_uuid() {
-        let id = Uuid::parse_str("550e8400-e29b-41d4-a716-446655440000").unwrap();
-        let expected_path = format!("{}/tmp/550e8400-e29b-41d4-a716-446655440000.js", env!("CARGO_MANIFEST_DIR"));
-
-        let result_path = get_script_tmp_directory(id);
-
-        assert_eq!(result_path, expected_path);
-    }
-
-    #[test]
-    fn test_get_script_tmp_directory_all_zeros_uuid() {
-        let id = Uuid::parse_str("00000000-0000-0000-0000-000000000000").unwrap();
-        let expected_path = format!("{}/tmp/00000000-0000-0000-0000-000000000000.js", env!("CARGO_MANIFEST_DIR"));
-
-        let result_path = get_script_tmp_directory(id);
-
-        assert_eq!(result_path, expected_path);
-    }
-
-    #[test]
-    fn test_get_script_tmp_directory_all_fs_uuid() {
-        let id = Uuid::parse_str("ffffffff-ffff-ffff-ffff-ffffffffffff").unwrap();
-        let expected_path = format!("{}/tmp/ffffffff-ffff-ffff-ffff-ffffffffffff.js", env!("CARGO_MANIFEST_DIR"));
-
-        let result_path = get_script_tmp_directory(id);
-
-        assert_eq!(result_path, expected_path);
-    }
 }
