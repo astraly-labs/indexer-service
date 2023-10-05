6c02cf36bd286f8168bf1896bb42bf8688550649
0xevolve <Artevolve@yahoo.com>
2023-10-05 13:32:31 +0200

    fix: remove clone

--- src/handlers/indexers/indexer_types/postgres.rs
+++ src/handlers/indexers/indexer_types/postgres.rs
@@ -11,7 +11,7 @@ impl Indexer for PostgresIndexer {
     async fn start(&self, indexer: &IndexerModel) -> u32 {
         let binary_file = format!("{}/{}", get_environment_variable("BINARY_BASE_PATH"), "sink-postgres");
         let postgres_connection_string = get_environment_variable("APIBARA_POSTGRES_CONNECTION_STRING");
-        let table_name = indexer.table_name.clone().expect("`table_name` not set for postgres indexer");
+        let table_name = indexer.table_name.as_ref().expect("`table_name` not set for postgres indexer");
         self.start_common(
             binary_file,
             indexer,
